da4aaad0e477b65398790e169e5eff782a04ac98
k-sable <91602075+k-sable@users.noreply.github.com>
2021-10-05 22:16:41 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,4 @@
+var result=67
+result+=1
+console.log("Hello World!")
+console.log("Result : "+result)
---
+++ timer.js
@@ -0,0 +1,19 @@
+console.log("Node js Application Started");
+
+//Reusable function
+
+var display=function(){
+
+	console.log("Welcome DBDA Students for Application Learning");
+
+};
+
+var show=function(){
+
+	console.log("Thank you!");
+
+};
+
+
+setInterval(display,2000);  //Inbuilt function.
+setInterval(show,5000); //Inbuilt function.
